38bb1fad7f1cf3ca83228adb69a1aae7626855c2
Finn Zentgraf <rubeustechtemp@gmail.com>
2019-04-22 12:48:35 +0200

    Implemented Basic nextjs server with typescript support

---
+++ src/pages/index.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export interface IIndexPageProps{}
+export interface IIndexPageState{}
+
+export default class IndexPage extends React.Component<IIndexPageProps,IIndexPageState>{
+    render(){
+        return <div>cornelius</div>;
+    }
+}
